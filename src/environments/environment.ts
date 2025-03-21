22ba3b9e9ddf839dcb02a694d81dfaf48cb69789
Millennium-Soft <millenniumsoft.contacto@gmail.com>
2025-03-21 13:51:11 -0500

    Plantilla inicial

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  URL_BACKEND: 'http://localhost:8080/plantilla',
+  firebase: {
+    apiKey: 'AIzaSyDUfTBcm6O2T4oOrJcD5ELLLKDTV14nY2s',
+    authDomain: 'elecciones-virtuales-msoft.firebaseapp.com',
+    projectId: 'elecciones-virtuales-msoft',
+    storageBucket: 'elecciones-virtuales-msoft.firebasestorage.app',
+    messagingSenderId: '868692128891',
+    appId: '1:868692128891:web:26f43d3c820cd33da45ede',
+    measurementId: 'G-5J99PZM1X3',
+  },
+};
